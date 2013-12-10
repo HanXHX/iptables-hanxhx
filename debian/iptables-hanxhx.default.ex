be34230ed790165c25d97658625a96641b8a096f
Emilien Mantel <hanx@emilien>
2013-12-10 11:11:35 +0100

    Debian packaging

---
+++ debian/iptables-hanxhx.default.ex
@@ -0,0 +1,10 @@
+# Defaults for iptables-hanxhx initscript
+# sourced by /etc/init.d/iptables-hanxhx
+# installed at /etc/default/iptables-hanxhx by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
